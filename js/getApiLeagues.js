bd3d89727985292cbc5d9ac800904b50e99f7082
Christian <christian_8_neus@hotmail.com>
2022-02-04 11:25:51 +0100

    cors

--- js/getApiLeagues.js
+++ js/getApiLeagues.js
@@ -8,40 +8,6 @@ let HEADERS = {
   'Access-Control-Max-Age': '8640'
 }
 
-//This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
-
-HEADERS['Access-Control-Allow-Origin'] = '*'
-HEADERS['Vary'] = 'Origin'
-
-exports.handler = async function (event, context) {
-  try {
-    if (event.httpMethod === 'OPTIONS') {
-      return { statusCode: '204', HEADERS }
-    }
-    if (event.httpMethod === 'POST') {
-        const body = JSON.parse(event.body)
-        //Your code goes here
-
-       return {
-         statusCode: 200,
-         body: 'Success',
-         HEADERS
-       } 
- 
-    }
-    return {
-      statusCode: 401,
-      HEADERS
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      statusCode: 500,
-      body: e.toString()
-    }
-  }
-}
-
 function getLeagues(id){
     fetch(`https://apiclient.besoccerapps.com/scripts/api/api.php?key=01da72449d29a7702aba403ec3fb4e98&tz=Europe/Madrid&format=json&req=categories&filter=all
     `)
